8074aae02b9174fe84189c664b968f26758b12b6
alex <AAnPolozov@sberbank.ru>
2021-08-19 13:27:45 +0300

    queue

--- Source/NodeSocket/Private/NodeSocketAC.cpp
+++ Source/NodeSocket/Private/NodeSocketAC.cpp
@@ -1,131 +1,121 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "NodeSocketAC.h"
 
 // Sets default values for this component's properties
 UNodeSocketAC::UNodeSocketAC()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 
-		// set the name of the socket
-	ClientSocketName = FString(TEXT("ue4-tcp-client"));
+    // set the name of the socket
+    ClientSocketName = FString(TEXT("ue4-tcp-client"));
 
-	// reset the socket
-	ClientSocket = nullptr;
+    // reset the socket
+    ClientSocket = nullptr;
 
-	// maximum buffer size
-	nBufferMaxSize = 2 * 1024 * 1024;	// 2 Mb
+    // maximum buffer size
+    nBufferMaxSize = 2 * 1024 * 1024; // 2 Mb
 }
-
 
 // Called when the game starts
 void UNodeSocketAC::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	// ...
-	
+    // ...
 }
-
 
 // Called every frame
-void UNodeSocketAC::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UNodeSocketAC::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    fMsgEvent();
+    // ...
 }
-
-
-
 
 void UNodeSocketAC::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	CloseSocket();
+    CloseSocket();
 }
-
-
 
 /**
  * Connects to server
  */
-void UNodeSocketAC::ConnectToServer(const FString& InIP, const int32 InPort)
+void UNodeSocketAC::ConnectToServer(const FString &InIP, const int32 InPort)
 {
 
-	// we form the connection address
-	RemoteAdress = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+    bool bIsValid =false;
+    // we form the connection address
+    this->RemoteAdress = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
 
-	UE_LOG(LogTemp, Log, TEXT("TCP address try to connect <%s:%d>"), *InIP, InPort);
+    UE_LOG(LogTemp, Log, TEXT("TCP address try to connect <%s:%d>"), *InIP, InPort);
 
-	bool bIsValid;
-	RemoteAdress->SetIp(*InIP, bIsValid);
-	RemoteAdress->SetPort(InPort);
+    this->RemoteAdress->SetIp(*InIP, bIsValid);
+    this->RemoteAdress->SetPort(InPort);
 
-	// we check the validity of the connection address
-	if (!bIsValid)
-	{
-		UE_LOG(LogTemp, Error, TEXT("TCP address is invalid <%s:%d>"), *InIP, InPort);
-		return;
-	}
+    // we check the validity of the connection address
+    if (!bIsValid)
+    {
+        UE_LOG(LogTemp, Error, TEXT("TCP address is invalid <%s:%d>"), *InIP, InPort);
+        return;
+    }
 
-	// Get the socket subsystem
-	ClientSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, ClientSocketName, false);
+    // Get the socket subsystem
+    this->ClientSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, ClientSocketName, false);
 
-	// Set the size of the send / receive buffer
-	ClientSocket->SetSendBufferSize(nBufferMaxSize, nBufferMaxSize);
-	ClientSocket->SetReceiveBufferSize(nBufferMaxSize, nBufferMaxSize);
+    // Set the size of the send / receive buffer
+    this->ClientSocket->SetSendBufferSize(this->nBufferMaxSize, this->nBufferMaxSize);
+    this->ClientSocket->SetReceiveBufferSize(this->nBufferMaxSize, this->nBufferMaxSize);
 
-	bIsConnected = ClientSocket->Connect(*RemoteAdress);
+    bIsConnected = this->ClientSocket->Connect(*this->RemoteAdress);
 
-	// if you connected Broadcast event
-	if (bIsConnected)
-	{
-		OnConnected.Broadcast();
-		// we say that we are ready to receive data
-		bShouldReceiveData = true;
-	}
+    // if you connected Broadcast event
+    if (bIsConnected)
+    {
+        this->OnConnected.Broadcast();
+        // we say that we are ready to receive data
+        bShouldReceiveData = true;
+    }
 
+    // Data listener
+    ClientConnectionFinishedFuture =
+        Async(EAsyncExecution::Thread,
+              [&]()
+              {
+                  uint32 BufferSize = 0;
+                  TArray<uint8> ReceiveBuffer;
+                  FString ResultString;
 
-	// Data listener
-	ClientConnectionFinishedFuture = Async(EAsyncExecution::Thread, [&]()
-		{
-			uint32 BufferSize = 0;
-			TArray<uint8> ReceiveBuffer;
-			FString ResultString;
+                  // we start an endless loop for receiving data
+                  while (bShouldReceiveData)
+                  {
+                      // if there is data
+                      if (this->ClientSocket->HasPendingData(BufferSize))
+                      {
+                          // set buffer size
+                          ReceiveBuffer.SetNumUninitialized(BufferSize);
 
-			// we start an endless loop for receiving data
-			while (bShouldReceiveData)
-			{
-				// if there is data
-				if (ClientSocket->HasPendingData(BufferSize))
-				{
-					// set buffer size
-					ReceiveBuffer.SetNumUninitialized(BufferSize);
+                          int32 Read = 0;
+                          this->ClientSocket->Recv(ReceiveBuffer.GetData(), ReceiveBuffer.Num(), Read);
 
-					int32 Read = 0;
-					ClientSocket->Recv(ReceiveBuffer.GetData(), ReceiveBuffer.Num(), Read);
+                          // send buffer to event
+                          this->OnReceivedBytes.Broadcast(ReceiveBuffer);
 
-					// send buffer to event
-					OnReceivedBytes.Broadcast(ReceiveBuffer);
+                          // send string to event
+                          // FString sMsg = fBytesToString(ReceiveBuffer);
+                          // OnReceivedStr.Broadcast(msgStr);
+                          this->aReserveQ.Enqueue(fBytesToString(ReceiveBuffer));
 
-					// send string to event
-					FString msgStr = fBytesToString(ReceiveBuffer);
-					OnReceivedStr.Broadcast(msgStr);
-
-					// log message
-					UE_LOG(LogTemp, Log, TEXT("Reserve data: %s"), *(fBytesToString(ReceiveBuffer)));
-
-				}
-				// skip 1 tick
-				ClientSocket->Wait(ESocketWaitConditions::WaitForReadOrWrite, FTimespan(1));
-			}
-		}
-	);
+                          // log message
+                      }
+                      // skip 1 tick
+                      this->ClientSocket->Wait(ESocketWaitConditions::WaitForReadOrWrite, FTimespan(1));
+                  }
+              });
 }
 
 /**
@@ -133,90 +123,90 @@ void UNodeSocketAC::ConnectToServer(const FString& InIP, const int32 InPort)
  * */
 void UNodeSocketAC::CloseSocket()
 {
-	// if there is a socket
-	if (ClientSocket)
-	{
-		// stop receiving data
-		bShouldReceiveData = false;
+    // if there is a socket
+    if (ClientSocket)
+    {
+        // stop receiving data
+        bShouldReceiveData = false;
 
-		// end the connection
-		ClientConnectionFinishedFuture.Get();
-		ClientSocket->Close();
-		ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->DestroySocket(ClientSocket);
-		ClientSocket = nullptr;
-		UE_LOG(LogTemp, Log, TEXT("UNodeSocketAC disconnect"));
-
-	}
+        // end the connection
+        ClientConnectionFinishedFuture.Get();
+        this->ClientSocket->Close();
+        ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->DestroySocket(this->ClientSocket);
+        this->ClientSocket = nullptr;
+        UE_LOG(LogTemp, Log, TEXT("UNodeSocketAC disconnect"));
+    }
 }
 
 /**
  * Sends bytes to the server
  * */
-bool UNodeSocketAC::Emit(const TArray<uint8>& Bytes)
+bool UNodeSocketAC::Emit(const TArray<uint8> &Bytes)
 {
-	bool resp = false;
+    bool resp = false;
 
-	// check if there is a connection
-	if (ClientSocket && ClientSocket->GetConnectionState() == SCS_Connected)
-	{
-		int32 BytesSent = 0; // how many bytes sent
-		resp = ClientSocket->Send(Bytes.GetData(), Bytes.Num(), BytesSent);
-	}
-	return resp;
+    // check if there is a connection
+    if (this->ClientSocket && this->ClientSocket->GetConnectionState() == SCS_Connected)
+    {
+        int32 BytesSent = 0; // how many bytes sent
+        resp = this->ClientSocket->Send(Bytes.GetData(), Bytes.Num(), BytesSent);
+    }
+    return resp;
 }
 
 /**
  * Sends bytes to the server
  * */
-bool UNodeSocketAC::EmitStr(const FString& str)
+bool UNodeSocketAC::EmitStr(const FString &str)
 {
-	bool resp = false;
-	UE_LOG(LogTemp, Log, TEXT("Try send msg: %s"), *str);
+    bool resp = false;
+    UE_LOG(LogTemp, Log, TEXT("Try send msg: %s"), *str);
 
-	// check if there is a connection
-	if (ClientSocket && ClientSocket->GetConnectionState() == SCS_Connected)
-	{
-		int32 BytesSent = 0; // how many bytes sent
-		
-		// convert string to any charset
-		FTCHARToUTF8 Converted(*str);
-		(uint8*)Converted.Get(), Converted.Length();
+    // check if there is a connection
+    if (ClientSocket && ClientSocket->GetConnectionState() == SCS_Connected)
+    {
+        int32 BytesSent = 0; // how many bytes sent
 
-		resp = ClientSocket->Send((uint8*)Converted.Get(), Converted.Length(), BytesSent);
-	}
-	return resp;
+        // convert string to any charset
+        FTCHARToUTF8 Converted(*str);
+        (uint8 *)Converted.Get(), Converted.Length();
+
+        resp = this->ClientSocket->Send((uint8 *)Converted.Get(), Converted.Length(), BytesSent);
+    }
+    return resp;
 }
-
 
 /**
 * Converts bytes to strings
 */
-FString UNodeSocketAC::fBytesToString(const TArray<uint8>& InArray)
+FString UNodeSocketAC::fBytesToString(const TArray<uint8> &InArray)
 {
-	FString ResultString;
-	FFileHelper::BufferToString(ResultString, InArray.GetData(), InArray.Num());
-	return ResultString;
+    FString ResultString;
+    FFileHelper::BufferToString(ResultString, InArray.GetData(), InArray.Num());
+    return ResultString;
 }
-
 
 /**
 * Converts strings to bytes
 */
 TArray<uint8> UNodeSocketAC::fStringToBytes(FString InString)
 {
-	TArray<uint8> ResultBytes;
-	ResultBytes.Append((uint8*)TCHAR_TO_UTF8(*InString), InString.Len());
+    TArray<uint8> ResultBytes;
+    ResultBytes.Append((uint8 *)TCHAR_TO_UTF8(*InString), InString.Len());
 
-	UE_LOG(LogTemp, Log, TEXT("str len: %d"), InString.Len());
-	UE_LOG(LogTemp, Log, TEXT("array len: %d"), ResultBytes.Num());
+    UE_LOG(LogTemp, Log, TEXT("str len: %d"), InString.Len());
+    UE_LOG(LogTemp, Log, TEXT("array len: %d"), ResultBytes.Num());
 
-	return ResultBytes;
+    return ResultBytes;
 }
 
-
-
-
-
-
-
-
+void UNodeSocketAC::fMsgEvent()
+{
+    FString s;
+    if (!this->aReserveQ.IsEmpty())
+    {
+        this->aReserveQ.Dequeue(s);
+        this->OnReceivedStr.Broadcast(s);
+        UE_LOG(LogTemp, Log, TEXT("Broadcast data: %s"), *(s));
+    }
+}
--- Source/NodeSocket/Public/NodeSocketAC.h
+++ Source/NodeSocket/Public/NodeSocketAC.h
@@ -18,8 +18,10 @@
 #include "IPAddress.h"
 #include "HAL/ThreadSafeBool.h"
 #include "Containers/StringConv.h"
-#include "NodeSocketAC.generated.h"
 
+#include "Containers/Queue.h"
+
+#include "NodeSocketAC.generated.h"
 
 // when there was a connection to the server
 UDELEGATE(BlueprintAuthorityOnly)
@@ -27,118 +29,112 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FNSEventConnect);
 
 // when did you get the bytes
 UDELEGATE(BlueprintAuthorityOnly)
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNSEventMsgB, const TArray<uint8>&, Bytes);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNSEventMsgB, const TArray<uint8> &, Bytes);
 
-
-// 
+//
 // when did you get the string
 UDELEGATE(BlueprintAuthorityOnly)
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNSEventMsgStr, const  FString&, Msg);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNSEventMsgStr, const FString &, Msg);
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class NODESOCKET_API UNodeSocketAC : public UActorComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UNodeSocketAC();
+public:
+    // Sets default values for this component's properties
+    UNodeSocketAC();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
 
-	FSocket* ClientSocket;
+    FSocket *ClientSocket;
 
-	// can receive data safe for thread
-	FThreadSafeBool bShouldReceiveData;
+    // can receive data safe for thread
+    FThreadSafeBool bShouldReceiveData;
 
-	// 
-	TFuture<void> ClientConnectionFinishedFuture;
+    //
+    TFuture<void> ClientConnectionFinishedFuture;
 
-	// server address
-	TSharedPtr<FInternetAddr> RemoteAdress;
+    // server address
+    TSharedPtr<FInternetAddr> RemoteAdress;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    // send queue
+    TQueue<FString> aSendQ;
 
+    // reserv queue
+    TQueue<FString> aReserveQ;
 
+    void fMsgEvent();
 
-	// received bytes from the server
-	UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category="AA_Net")
-		FNSEventMsgB OnReceivedBytes;
+public:
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
-	// received a line from the server
-	UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category = "AA_Net")
-		FNSEventMsgStr OnReceivedStr;
+    // received bytes from the server
+    UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category = "AA_Net")
+    FNSEventMsgB OnReceivedBytes;
 
-	// runs when connected to the server
-	UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category = "AA_Net")
-		FNSEventConnect OnConnected;
+    // received a line from the server
+    UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category = "AA_Net")
+    FNSEventMsgStr OnReceivedStr;
 
-	// buffer size in bytes
-	UPROPERTY(BlueprintReadWrite, Category = "AA_Net")
-	int32 nBufferMaxSize;
+    // runs when connected to the server
+    UPROPERTY(BlueprintReadWrite, BlueprintAssignable, BlueprintCallable, Category = "AA_Net")
+    FNSEventConnect OnConnected;
 
+    // buffer size in bytes
+    UPROPERTY(BlueprintReadWrite, Category = "AA_Net")
+    int32 nBufferMaxSize;
 
-	/**
+    /**
 	 * Converts bytes to string
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA_Net")
-		FString fBytesToString(const TArray<uint8>& InArray);
+    UFUNCTION(BlueprintCallable, Category = "AA_Net")
+    FString fBytesToString(const TArray<uint8> &InArray);
 
-
-	/**
+    /**
 	 * Converts string to bytes
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA_Net")
-		TArray<uint8> fStringToBytes(FString InString);
+    UFUNCTION(BlueprintCallable, Category = "AA_Net")
+    TArray<uint8> fStringToBytes(FString InString);
 
+    // connection name
+    FString ClientSocketName;
 
+    // sign that connected
+    bool bIsConnected;
 
-	// connection name
-	FString ClientSocketName;
-
-	
-
-	// sign that connected
-	bool bIsConnected;
-
-
-	/**
+    /**
 	 * Connects over TCP to the server
 	 *
 	 * @param InIP ip4 address where we connect
 	 * @param InPort TCP connection port
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA")
-		void ConnectToServer(const FString& InIP = TEXT("127.0.0.1"), const int32 InPort = 3000);
+    UFUNCTION(BlueprintCallable, Category = "AA")
+    void ConnectToServer(const FString &InIP = TEXT("127.0.0.1"), const int32 InPort = 3000);
 
-	/**
+    /**
 	 * Closes the connection
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA")
-		void CloseSocket();
+    UFUNCTION(BlueprintCallable, Category = "AA")
+    void CloseSocket();
 
-	/**
+    /**
 	 * Sends bytes to the server
 	 *
 	 * @param Message	Bytes
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA")
-		bool Emit(const TArray<uint8>& Bytes);
+    UFUNCTION(BlueprintCallable, Category = "AA")
+    bool Emit(const TArray<uint8> &Bytes);
 
-
-	/**
+    /**
 	 * Sends a string to the server
 	 *
 	 * @param Message	Bytes
 	 */
-	UFUNCTION(BlueprintCallable, Category = "AA")
-		bool EmitStr(const FString& str);
-
-		
+    UFUNCTION(BlueprintCallable, Category = "AA")
+    bool EmitStr(const FString &str);
 };
